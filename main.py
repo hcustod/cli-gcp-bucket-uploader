dd4336f19d4516faa836158c81484ef350f7df9d
hcustod <111096105+hcustod@users.noreply.github.com>
2023-04-26 23:36:57 -0400

    some changes

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import click
+import logging
+from google.cloud import storage
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings')
-@click.option('--name', prompt='Your name', help='The person to greet')
 
-def hello(count, name): 
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+@click.argument("file_names", nargs=-1)
+def upload_files(file_names):
+    client = storage.Client()
+
+    bucket_name = "cli-file-uploader-dump"
+    bucket = client.get_bucket(bucket_name)
+
+    try:
+        blobs = bucket.list_blobs()
+        for blob in blobs:
+            print(blob.name)
+
+        for file_name in file_names:
+            print(f"Uploading file {file_name}")
+
+    except Exception as e:
+        logging.exception("Error Uploading files: %s", str(e))
+        print("Error uploading files: %s" % str(e))
 
 if __name__ == '__main__':
-    hello()
+    upload_files()
 
 
-    
+
